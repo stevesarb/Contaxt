9448b77754d93c1ceb18d8162afcb32b8434f713
Steven Sarber <sesarber@gmail.com>
2024-09-15 09:58:39 -0700

    Finished Add Group Modal

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function show_add_group_modal() {
 
     const main = document.getElementsByTagName( "main" );
     const add_group_modal = document.getElementById( "add_group_modal" );
+
+    main.classList.add( "blur-sm" );
+    add_group_modal.classList.remove( "hidden" );
 }
